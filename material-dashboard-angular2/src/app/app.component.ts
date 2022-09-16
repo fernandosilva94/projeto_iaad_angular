3ee5f8b2e8f632568a919bb55662349e1c4c7cd7
Victor160197 <jvncruz1697@hotmail.com>
2022-09-16 00:00:46 -0300

    Alterações na pagina do dashboard e add developer

--- material-dashboard-angular2/src/app/app.component.ts
+++ material-dashboard-angular2/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component} from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', //'datepicker-custom-icon-example',
+  templateUrl: './app.component.html', //'datepicker-custom-icon-example.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
